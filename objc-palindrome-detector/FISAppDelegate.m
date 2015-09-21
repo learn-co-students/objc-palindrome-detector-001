1b8910bb47b0cb7d8b12f98dd2f132dad41a4fde
onowahoo <onowahoo@gmail.com>
2015-09-21 16:46:10 -0400

    Palindrome Project

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -5,22 +5,61 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-
-    /**
-     
-     * Write your check code here.
-     
-     */
     
-    // do not alter
-    return YES;  //
-}   ///////////////
+    NSString *palindrome = @"palindrome";
+    NSString *reversed = [self stringByReversingString:palindrome];
+    NSLog(@"%@, %@", palindrome, reversed);
+    
+    NSString *racecar = @"racecar";
+    bool racecarIsPalindrome = [self stringIsPalindrome:racecar];
+    NSLog(@"%d : %@",racecarIsPalindrome, racecar);
+    
+    NSString *bob = @"never odd or even";
+    bool bobIsPalindrome = [self stringIsPalindrome:bob];
+    NSLog(@"%d : %@",bobIsPalindrome, bob);
+    
+    NSString *fleeToMe = @"Flee to me, remote elf.";
+    bool fleeIsPalindrome = [self stringIsPalindrome:fleeToMe];
+    NSLog(@"%d : %@",fleeIsPalindrome,fleeToMe);
+    
+    return YES;
+}
 
-/**
- 
- * Implement your methods here.
- 
- */
+
+- (BOOL)stringIsPalindrome:(NSString *)string{
+    NSArray *punctuations = @[@".",@",",@"!",@"?",@":",@";"];
+   
+    for (NSUInteger i =0; i<[punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        string = [string stringByReplacingOccurrencesOfString:punctuation withString:@""];
+        
+    }
+    
+    string = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *reverse = [self stringByReversingString:string];
+    reverse = [reverse lowercaseString];
+    string = [string lowercaseString];
+    
+    BOOL stringIsEqualToReverse = [reverse isEqualToString:string];
+    
+    
+    return stringIsEqualToReverse;
+}
+
+
+- (NSString *)stringByReversingString:(NSString *)string{
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length];i>0; i--) {
+        NSUInteger index = i-1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C",c];
+    }
+    return result;
+}
+
+
+
 
 @end
