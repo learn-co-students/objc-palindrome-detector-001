82e92d7d2ba96e4d27ac28dcc3deb57e5b2a4979
Abdoulaye Diallo <ConnectX@Abdoulayes-MacBook-Pro.local>
2016-06-08 12:21:16 -0400

    worked to the lab

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -36,12 +36,20 @@ NSString *palindrome=@"mamam";
 
 - (BOOL)stringIsPalindrome:(NSString *)string {
     
+    NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";" ];
+    NSString *withoutPunctuation = [string copy];
     
-    NSString *lowercase = [string lowercaseString];
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    NSString *lowercase = [spaceless lowercaseString];
     
     NSString *reverse = [self stringByReversingString:lowercase];
     
-    NSLog(@"lowercase: %@, reverse: %@", lowercase, reverse);
     BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
     
     return stringIsEqualToReverse;
