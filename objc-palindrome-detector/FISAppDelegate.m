ed3f0dd834f4240b5676da2c51111c86bcf95f0e
Abdoulaye Diallo <ConnectX@Abdoulayes-MacBook-Pro.local>
2016-06-08 12:17:58 -0400

    worked to the lab

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -7,20 +7,58 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
-    /**
-     
-     * Write your check code here.
-     
-     */
+    NSString *racecar = @"racecar";
     
+    BOOL racecarIsPalindrome=[self stringIsPalindrome:racecar];
+    NSLog(@"%d : %@", racecarIsPalindrome, racecar);
+    
+NSString *palindrome=@"mamam";
+    BOOL palindromeIsPalindrome = [self stringIsPalindrome:palindrome];
+    NSLog(@"%d : %@", palindromeIsPalindrome, palindrome);
+    
+    
+    
+    // single word, with uppercase
+    NSString *bob = @"Bob";
+    BOOL bobIsPalindrome = [self stringIsPalindrome:bob];
+    NSLog(@"%d : %@", bobIsPalindrome, bob);
+    
+    NSString *kanakanak = @"Kanakanak";
+    BOOL kanakanakIsPalindrome = [self stringIsPalindrome:kanakanak];
+    NSLog(@"%d : %@", kanakanakIsPalindrome, kanakanak);
+    
+    NSString *aibohphobia = @"Aibohphobia";
+    BOOL aibohphobiaIsPalindrome = [self stringIsPalindrome:aibohphobia];
+    NSLog(@"%d : %@", aibohphobiaIsPalindrome, aibohphobia);
     // do not alter
     return YES;  //
 }   ///////////////
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    
+    NSString *lowercase = [string lowercaseString];
+    
+    NSString *reverse = [self stringByReversingString:lowercase];
+    
+    NSLog(@"lowercase: %@, reverse: %@", lowercase, reverse);
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+    }
+    
+    return result;
+
+}
 
 @end
